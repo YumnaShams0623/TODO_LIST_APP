d2bb2f593074b54e318f2c1aff4207fbe3a0f169
YumnaShams0623 <yumniijan26471@gmial.com>
2024-09-26 13:42:31 +0500

    making a function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 var input = document.querySelector('#input');
+var list = document.querySelector('#list');
+input.addEventListener("keyup",function(event){
+    if(event.key == Enter){
+        addItem(this.value)
+        this.value = ""
+    }
+})
+var addItem = (input) => {
+    var listItem = document.createElement("li");
+    listItem.innerHTML = `${input}<i><i>`;
+
+    list.appendChild(listItem);
+}
