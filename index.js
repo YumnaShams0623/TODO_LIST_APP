329f0959cc7d3f17c74444b770386e9f9b0d4827
YumnaShams0623 <yumniijan26471@gmial.com>
2024-09-26 16:34:09 +0500

    added button

---
+++ index.js
@@ -0,0 +1,7 @@
+function addTask() {
+    var taskInput = document.getElementById('input');
+    var taskList = document.getElementById('list');
+
+    var li = document.createElement('li');
+    li.textContent = input.value;
+}
